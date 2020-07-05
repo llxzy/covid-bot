c3f64b6f0af2f3062b3190958318eb56f88c0606
llxzy <snowcrash@tuta.io>
2020-07-05 13:10:32 +0200

    (#003) Added config file basic setup, error handling for info, cleaned up some code

--- bot.py
+++ bot.py
@@ -2,20 +2,20 @@ import csv
 import datetime
 from dateutil.parser import parse
 import discord
+import os
 import yaml
 import urllib.request
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')
+bot.remove_command('help')
 
 LAST_ARCHIVED = datetime.date(2020, 7, 4)
 DATA_URL = "https://mapa.covid.chat/export/csv"
 
 #TODO
-# config file
 # date formatting
 # arguments for command
-# help function
 
 def get_csv() -> bool:
     """
@@ -38,33 +38,69 @@ def parse_csv() -> list:
         for row in csv_data:
             dt.append(row)
     return dt
-            # if datetime.datetime(2020, 7, 3) == parse(i["Datum"], dayfirst=True):
+
+
+def format_data(date_str: str, row: dict) -> discord.Embed:
+    embed = discord.Embed(title=f"Data for {date_str}")
+    embed.add_field(name="Confirmed", value=row["Pocet potvrdenych"])
+    embed.add_field(name="Active", value=row["Pocet aktivnych"])
+    embed.add_field(name="Cured", value=row["Pocet vyliecenych"])
+    embed.add_field(name="New tested", value=row["Dennych testov"])
+    embed.add_field(name="New cases", value=row["Dennych prirastkov"])
+    embed.add_field(name="Total deaths", value=row["Pocet umrti"])
+    return embed
+
+"""
+----------------------------------------------------------------
+"""
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Game(name="!help for a list of commands"))
+
+
+@bot.command(name='help')
+async def help_msg(ctx) -> None:
+    m_embed = discord.Embed()
+    m_embed.add_field(name="Commands", value="help, source, info (WIP)")
+    await ctx.send(embed=m_embed)
+
+
+@bot.command(name='source')
+async def source_msg(ctx) -> None:
+    m_embed = discord.Embed()
+    m_embed.add_field(name="Source code available at:", value="https://github.com/llxzy/covid-bot")
+    await ctx.send(embed=m_embed)
+
 
 @bot.command(name='info')
-async def info(ctx, year, month, day):
-    #TODO set defaults for y m d
+async def info(ctx, year: str, month: str, day: str):
     if datetime.date.today() != LAST_ARCHIVED:
         get_csv()
     data_dict = parse_csv()
-    date_string = f"{year}-{month}-{day}"
     for entry in data_dict:
-        if parse(date_string) == parse(entry["Datum"], dayfirst=True):
-            today = entry["Datum"]
-            m_embed = discord.Embed(title=f"Data for {date_string}")
-            #TODO move to separate func
-            m_embed.add_field(name="Total cases", value=entry["Pocet potvrdenych"])
-            m_embed.add_field(name="New cases", value=entry["Dennych prirastkov"])
-            m_embed.add_field(name="Daily tested", value=entry["Dennych testov"])
-            #await ctx.send(f"The value for today is {today}")
-            await ctx.send(embed=m_embed)
-            return
-    ctx.send("Data for selected day not found.")
+        if parse(f"{year}-{month}-{day}") == parse(entry["Datum"], dayfirst=True):
+            formatted_date_string = datetime.date(int(year), int(month), int(day)).strftime("%d/%m/%Y")
+            await ctx.send(embed=format_data(formatted_date_string, entry))
+    await ctx.send(embed=discord.Embed(title="Data for selected day not found"))
 
+
+@info.error
+async def info_error(ctx, error):
+    await ctx.send("```Usage: !info <year> <month> <day>```")
 
 
 def main():
-    # todo Setup connection string on first startup, else bot.run(connection_string)
-    bot.run('CONNECTION STRING')
+    if not os.path.isfile("config.yaml"):
+        print("No configuration file found.")
+        print("Please enter your connection string into the file and rerun the program.")
+        data = {"connection_string" : ""}
+        with open("config.yaml", "w") as new_file:
+            yaml.dump(data, new_file)
+    else:
+        with open("config.yaml", "r") as config_file:
+            config = yaml.safe_load(config_file)
+        bot.run(config["connection_string"])
 
 if __name__ == "__main__":
     main()
